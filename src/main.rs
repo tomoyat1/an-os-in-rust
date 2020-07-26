25cd4e4e618b2fa2e39186b9416e0ea5cda83c90
Tomoya Tabuchi <t@tomoyat1.com>
2020-07-26 19:51:29 +0900

    Clear background on boot

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,41 @@
 #![no_std]
 #![no_main]
 #![feature(abi_efiapi)]
-
 #![feature(alloc)]
 extern crate alloc;
-extern crate uefi;
-extern  crate uefi_services;
 extern crate compiler_builtins;
+extern crate uefi;
+extern crate uefi_services;
 
-
-use uefi::prelude::*;
+use alloc::vec::*;
 use log::info;
+use uefi::prelude::*;
+use uefi::proto::console::gop::{BltOp, BltPixel, GraphicsOutput};
+use uefi::table::boot::{EventType, SearchType, TimerTrigger, Tpl};
 
 #[entry]
 fn efi_main(_handle: Handle, system_table: SystemTable<Boot>) -> Status {
     // Initialize logging.
     uefi_services::init(&system_table);
+
+    // clear screen
+    {
+        let bs = system_table.boot_services();
+        let gop = bs
+            .locate_protocol::<GraphicsOutput>()
+            .expect("Graphics Output Protocol support is required!");
+        let gop = gop.expect("warnings occured when opening GOP");
+        let gop = unsafe { &mut *gop.get() };
+
+        let mode_info = gop.current_mode_info();
+        let (width_px, height_px) = mode_info.resolution();
+        gop.blt(BltOp::VideoFill {
+            color: BltPixel::new(0x35, 0x33, 0x2b),
+            dest: (0, 0),
+            dims: (width_px, height_px),
+        });
+    }
+
     info!("Logging initialized :)");
 
     // Print out the UEFI revision number
