ef8e1babbb6849d89399642afe439eea4d14d0a3
Tomoya Tabuchi <t@tomoyat1.com>
2020-07-26 14:24:39 +0900

    Add minimal compilable code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(abi_efiapi)]
+
+#![feature(alloc)]
+extern crate alloc;
+extern crate uefi;
+extern  crate uefi_services;
+extern crate compiler_builtins;
+
+
+use uefi::prelude::*;
+use log::info;
+
+#[entry]
+fn efi_main(_handle: Handle, system_table: SystemTable<Boot>) -> Status {
+    // Initialize logging.
+    uefi_services::init(&system_table);
+    info!("Logging initialized :)");
+
+    // Print out the UEFI revision number
+    {
+        let rev = system_table.uefi_revision();
+        let (major, minor) = (rev.major(), rev.minor());
+
+        info!("UEFI revision");
+        info!("UEFI {}.{}", major, minor);
+    }
+
+    // Proceed to bootstrapping the kernel.
+    loop {}
 }
