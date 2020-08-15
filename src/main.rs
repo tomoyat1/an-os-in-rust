417e6fe2f1ddfc8e13ec2d242342831df26dc6fd
Tomoya Tabuchi <t@tomoyat1.com>
2020-08-15 13:10:36 +0900

    Add page map probing

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use core::fmt::Write;
 
 use log::info;
 use uefi::prelude::*;
-use uefi::table::boot::{EventType, SearchType, TimerTrigger, Tpl};
+use uefi::table::boot::{EventType, MemoryType, SearchType, TimerTrigger, Tpl};
 
 pub mod framebuffer;
 pub use crate::framebuffer::Framebuffer;
@@ -29,6 +29,24 @@ fn efi_main(_handle: Handle, system_table: SystemTable<Boot>) -> Status {
         fb.init().expect("failed to initialize framebuffer");
         system_table.boot_services().stall(1000000);
         writeln!(fb, "Hello, World!");
+
+        let bt = system_table.boot_services();
+        let mem_map_size = 2 * bt.memory_map_size();
+        let mut buf: Vec<u8> = Vec::with_capacity(mem_map_size);
+        unsafe {
+            buf.set_len(mem_map_size);
+        }
+        let (_k, mut desc_iter) = bt
+            .memory_map(&mut buf)
+            .expect("Failed to retrieve UEFI memory map")
+            .expect("warnings in retriving UEFI memory map");
+        writeln!(fb, "efi: usable memory ranges ({} total)", desc_iter.len());
+        const EFI_PAGE_SIZE: u64 = 0x1000;
+        for (j, descriptor) in desc_iter.enumerate() {
+            let size = descriptor.page_count * EFI_PAGE_SIZE;
+            let end_address = descriptor.phys_start + size;
+            writeln!(fb, "{:?}: {:#x} - {:#x} ({} KiB)", descriptor.ty, descriptor.phys_start, end_address, size);
+        }
 
         let mut cr0: u32;
         let mut cr3: u64;
@@ -56,6 +74,7 @@ fn efi_main(_handle: Handle, system_table: SystemTable<Boot>) -> Status {
         writeln!(fb, "page directory base: {:x}", (cr3 & 0xFFFFFFFFFFFFF000) >> 12);
 
         writeln!(fb, "PAE enabled: {:x}", (cr4 & (1 << 5)) >> 5);
+        writeln!(fb, "LA57 enabled: {:x}", (cr4 & (1 << 12)) >> 12);
 
         writeln!(fb, "efer: {:x}", efer);
         writeln!(fb, "long mode enabled: {:x}", (efer & (1 << 8)) >> 8);
