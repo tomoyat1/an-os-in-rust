a7c3689a2db61acb6859226fbce1a4afa0f1b52f
Tomoya Tabuchi <t@tomoyat1.com>
2020-09-15 12:05:29 +0900

    Add WithSpinLock::lock()

--- src/locking/spinlock.rs
+++ src/locking/spinlock.rs
@@ -3,9 +3,13 @@ pub struct WithSpinLock<A> {
 }
 
 impl<A> WithSpinLock<A> {
-    pub const fn new(a: A) -> Self<A> {
+    pub const fn new(a: A) -> Self {
         Self {
             inner: spin::Mutex::new(a),
         }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
     }
 }
