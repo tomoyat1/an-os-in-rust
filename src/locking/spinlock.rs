9d0452a6246a671c81ddee61d4e5271c17a5a00e
Tomoya Tabuchi <t@tomoyat1.com>
2025-09-24 14:06:59 +0900

    spinlock.rs: Do not enable interrupts if they were disabled when acquiring a spinlock.

--- src/locking/spinlock.rs
+++ src/locking/spinlock.rs
@@ -4,6 +4,8 @@ use core::ops::{Deref, DerefMut};
 use core::ptr::drop_in_place;
 use spin::MutexGuard;
 
+use crate::kernel::sched;
+
 pub struct WithSpinLock<A> {
     inner: SyncUnsafeCell<spin::Mutex<A>>,
 }
@@ -17,26 +19,43 @@ impl<A> WithSpinLock<A> {
 
     pub fn lock(&self) -> WithSpinLockGuard<A> {
         // Disable interrupts to prevent deadlocks.
-        unsafe { asm!("cli") };
+        let rflags: u64;
+        unsafe {
+            asm!(
+            "pushfq",
+            "pop {reg}",
+            reg = out(reg) rflags
+            )
+        }
+        let interrupt_enabled = (rflags >> 9) & 1 != 0;
+        if interrupt_enabled {
+            unsafe { asm!("cli") };
+        }
 
         // SAFETY: Access exclusivity is guaranteed through the spinlock Mutex.
         let spin_mutex = unsafe { &mut *self.inner.get() };
         let guard = spin_mutex.lock();
 
-        WithSpinLockGuard { inner: guard }
+        WithSpinLockGuard {
+            inner: guard,
+            interrupt_enabled,
+        }
     }
 }
 
 pub struct WithSpinLockGuard<'a, T> {
     inner: MutexGuard<'a, T>,
+    interrupt_enabled: bool,
 }
 
 impl<'a, T> Drop for WithSpinLockGuard<'a, T> {
     fn drop(&mut self) {
         unsafe {
             // self.inner should be dropped before sti
-            drop_in_place(&mut self.inner as *mut MutexGuard<T>);
-            asm!("sti");
+            drop_in_place(&mut self.inner);
+            if self.interrupt_enabled {
+                asm!("sti");
+            }
         }
     }
 }
