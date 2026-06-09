0d795682c7d5f46a8e244046d74fcd9b482e13e9
Tomoya Tabuchi <t@tomoyat1.com>
2026-06-09 21:32:04 +0900

    Implement Deref for SyncMutPointer

--- util/src/pointer.rs
+++ util/src/pointer.rs
@@ -1,6 +1,16 @@
+use core::ops::Deref;
+
 pub struct SyncMutPointer<T>(pub *mut T);
 unsafe impl<T> Send for SyncMutPointer<T> {}
 unsafe impl<T> Sync for SyncMutPointer<T> {}
+
+impl<T> Deref for SyncMutPointer<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe { &*self.0 }
+    }
+}
 
 impl<T> From<&SyncMutPointer<T>> for usize {
     fn from(base: &SyncMutPointer<T>) -> usize {
