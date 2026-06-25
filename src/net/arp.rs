0b53511fb39ad13840d4ffad2c673e455be5113f
Tomoya Tabuchi <t@tomoyat1.com>
2026-06-25 21:28:33 +0900

    Wrapper style ARP struct, like ethernet::Frame

--- src/net/arp.rs
+++ src/net/arp.rs
@@ -18,101 +18,271 @@ impl From<u16> for Opcode {
     }
 }
 
-struct ARP {
-    hardware_address_space: u16,
-    protocol_address_space: u16,
-    opcode: u16,
-    sender_hardware_address: Vec<u8>,
-    sender_protocol_address: Vec<u8>,
-    target_hardware_address: Vec<u8>,
-    target_protocol_address: Vec<u8>,
-}
-
-impl ARP {
-    fn from_bytes(bytes: &[u8]) -> Self {
-        let (hrd, remaining) = bytes.split_at(size_of::<u16>());
-        let hardware_address_space = u16::from_be_bytes(hrd.try_into().unwrap());
-
-        let (pro, remaining) = remaining.split_at(size_of::<u16>());
-        let protocol_address_space = u16::from_be_bytes(pro.try_into().unwrap());
-
-        let (hln, remaining) = remaining.split_at(size_of::<u8>());
-        let hardware_address_length = u8::from_be_bytes(hln.try_into().unwrap());
-        let mut sender_hardware_address = Vec::with_capacity(hardware_address_length as usize);
-        let mut target_hardware_address = Vec::with_capacity(hardware_address_length as usize);
-
-        let (pln, remaining) = remaining.split_at(size_of::<u8>());
-        let protocol_address_length = u8::from_be_bytes(pln.try_into().unwrap());
-        let mut sender_protocol_address = Vec::with_capacity(protocol_address_length as usize);
-        let mut target_protocol_address = Vec::with_capacity(protocol_address_length as usize);
-
-        let (op, remaining) = remaining.split_at(size_of::<u16>());
-        let opcode = u16::from_be_bytes(op.try_into().unwrap());
-
-        let (sha, remaining) = remaining.split_at(hardware_address_length as usize);
-        sender_hardware_address.extend_from_slice(sha);
-
-        let (spa, remaining) = remaining.split_at(protocol_address_length as usize);
-        sender_protocol_address.extend_from_slice(spa);
-
-        let (tha, remaining) = remaining.split_at(hardware_address_length as usize);
-        target_hardware_address.extend_from_slice(tha);
-
-        let (tpa, remaining) = remaining.split_at(protocol_address_length as usize);
-        target_protocol_address.extend_from_slice(tpa);
-
-        Self {
-            hardware_address_space,
-            protocol_address_space,
-            opcode,
-            sender_hardware_address,
-            sender_protocol_address,
-            target_hardware_address,
-            target_protocol_address,
+impl From<Opcode> for u16 {
+    fn from(value: Opcode) -> Self {
+        match value {
+            Opcode::Request => 1,
+            Opcode::Reply => 2,
+            Opcode::Unknown => 0,
         }
-    }
-
-    fn reply(self, sha: MACAddress) -> Self {
-        Self {
-            opcode: 2, // TODO: make this a const
-            sender_hardware_address: Vec::from(<[u8; 6]>::from(sha)),
-            sender_protocol_address: self.target_protocol_address,
-            target_hardware_address: self.sender_hardware_address,
-            target_protocol_address: self.sender_protocol_address,
-            ..self
-        }
-    }
-    fn write_bytes(&self, buf: &mut [u8]) -> usize {
-        let mut written = 0usize;
-
-        let mut put = |src: &[u8]| {
-            buf[written..written + src.len()].copy_from_slice(src);
-            written += src.len();
-        };
-
-        put(&u16::to_be_bytes(self.hardware_address_space));
-        put(&u16::to_be_bytes(self.protocol_address_space));
-        put(&u8::to_be_bytes(self.sender_hardware_address.len() as u8));
-        put(&u8::to_be_bytes(self.sender_protocol_address.len() as u8));
-        put(&u16::to_be_bytes(self.opcode));
-        put(&self.sender_hardware_address);
-        put(&self.sender_protocol_address);
-        put(&self.target_hardware_address);
-        put(&self.target_protocol_address);
-
-        written
     }
 }
 
-pub fn reply_writer(recv_bytes: &[u8], sha: MACAddress) -> impl FnOnce(&mut [u8]) -> usize {
-    let received = ARP::from_bytes(recv_bytes);
-    let reply = match Opcode::from(received.opcode) {
-        Opcode::Request => Some(received.reply(sha)),
-        _ => None,
-    };
+#[repr(transparent)]
+pub(crate) struct ARP<'a> {
+    bytes: &'a [u8],
+}
 
-    move |buf| match &reply {
-        Some(reply) => reply.write_bytes(buf),
-        None => 0,
+impl<'a> ARP<'a> {
+    pub fn from_bytes(bytes: &'a [u8]) -> Self {
+        Self { bytes }
+    }
+
+    pub fn len(&self) -> usize {
+        self.bytes.len()
+    }
+
+    pub fn as_bytes(&self) -> &[u8] {
+        self.bytes
+    }
+
+    pub fn hardware_address_space(&self) -> u16 {
+        let mut bytes = [0u8; 2];
+        bytes.copy_from_slice(&self.bytes[0..2]);
+        u16::from_be_bytes(bytes)
+    }
+
+    pub fn protocol_address_space(&self) -> u16 {
+        let mut bytes = [0u8; 2];
+        bytes.copy_from_slice(&self.bytes[2..4]);
+        u16::from_be_bytes(bytes)
+    }
+
+    pub fn hardware_address_length(&self) -> u8 {
+        self.bytes[4]
+    }
+
+    pub fn protocol_address_length(&self) -> u8 {
+        self.bytes[5]
+    }
+
+    pub fn opcode(&self) -> u16 {
+        let mut bytes = [0u8; 2];
+        bytes.copy_from_slice(&self.bytes[6..8]);
+        u16::from_be_bytes(bytes)
+    }
+
+    pub fn sender_hardware_address(&self) -> &[u8] {
+        &self.bytes[8..8 + self.hardware_address_length() as usize]
+    }
+
+    pub fn sender_protocol_address(&self) -> &[u8] {
+        let start = 8 + self.hardware_address_length() as usize;
+        &self.bytes[start..start + self.protocol_address_length() as usize]
+    }
+
+    pub fn target_hardware_address(&self) -> &[u8] {
+        let start =
+            8 + self.hardware_address_length() as usize + self.protocol_address_length() as usize;
+        &self.bytes[start..start + self.hardware_address_length() as usize]
+    }
+
+    pub fn target_protocol_address(&self) -> &[u8] {
+        let start = 8
+            + self.hardware_address_length() as usize
+            + self.protocol_address_length() as usize
+            + self.hardware_address_length() as usize;
+        &self.bytes[start..start + self.protocol_address_length() as usize]
+    }
+}
+
+pub(crate) struct ARPBuilder<'a, S: builder::Step> {
+    buf: &'a mut [u8],
+    pos: usize,
+    _phantom: core::marker::PhantomData<S>,
+}
+
+impl<'a> ARPBuilder<'a, builder::HardwareAddressSpace> {
+    pub fn new(buf: &'a mut [u8]) -> ARPBuilder<'a, builder::HardwareAddressSpace> {
+        ARPBuilder {
+            buf,
+            pos: 0,
+            _phantom: core::marker::PhantomData,
+        }
+    }
+
+    pub fn hardware_address_space(
+        mut self,
+        space: u16,
+    ) -> ARPBuilder<'a, builder::ProtocolAddressSpace> {
+        self.buf[self.pos..self.pos + 2].copy_from_slice(&space.to_be_bytes());
+        self.pos += 2;
+        ARPBuilder {
+            buf: self.buf,
+            pos: self.pos,
+            _phantom: core::marker::PhantomData,
+        }
+    }
+}
+
+impl<'a> ARPBuilder<'a, builder::ProtocolAddressSpace> {
+    pub fn protocol_address_space(
+        mut self,
+        space: u16,
+    ) -> ARPBuilder<'a, builder::HardwareAddressLength> {
+        self.buf[self.pos..self.pos + 2].copy_from_slice(&space.to_be_bytes());
+        self.pos += 2;
+        ARPBuilder {
+            buf: self.buf,
+            pos: self.pos,
+            _phantom: core::marker::PhantomData,
+        }
+    }
+}
+
+impl<'a> ARPBuilder<'a, builder::HardwareAddressLength> {
+    pub fn hardware_address_length(
+        mut self,
+        length: u8,
+    ) -> ARPBuilder<'a, builder::ProtocolAddressLength> {
+        self.buf[self.pos] = length;
+        self.pos += 1;
+        ARPBuilder {
+            buf: self.buf,
+            pos: self.pos,
+            _phantom: core::marker::PhantomData,
+        }
+    }
+}
+
+impl<'a> ARPBuilder<'a, builder::ProtocolAddressLength> {
+    pub fn protocol_address_length(mut self, length: u8) -> ARPBuilder<'a, builder::Opcode> {
+        self.buf[self.pos] = length;
+        self.pos += 1;
+        ARPBuilder {
+            buf: self.buf,
+            pos: self.pos,
+            _phantom: core::marker::PhantomData,
+        }
+    }
+}
+
+impl<'a> ARPBuilder<'a, builder::Opcode> {
+    pub fn opcode(mut self, opcode: Opcode) -> ARPBuilder<'a, builder::SenderHardwareAddress> {
+        self.buf[self.pos..self.pos + 2].copy_from_slice(&u16::from(opcode).to_be_bytes());
+        self.pos += 2;
+        ARPBuilder {
+            buf: self.buf,
+            pos: self.pos,
+            _phantom: core::marker::PhantomData,
+        }
+    }
+}
+
+impl<'a> ARPBuilder<'a, builder::SenderHardwareAddress> {
+    pub fn sender_hardware_address(
+        mut self,
+        address: &[u8],
+    ) -> ARPBuilder<'a, builder::SenderProtocolAddress> {
+        self.buf[self.pos..self.pos + address.len()].copy_from_slice(address);
+        self.pos += address.len();
+        ARPBuilder {
+            buf: self.buf,
+            pos: self.pos,
+            _phantom: core::marker::PhantomData,
+        }
+    }
+}
+
+impl<'a> ARPBuilder<'a, builder::SenderProtocolAddress> {
+    pub fn sender_protocol_address(
+        mut self,
+        address: &[u8],
+    ) -> ARPBuilder<'a, builder::TargetHardwareAddress> {
+        self.buf[self.pos..self.pos + address.len()].copy_from_slice(address);
+        self.pos += address.len();
+        ARPBuilder {
+            buf: self.buf,
+            pos: self.pos,
+            _phantom: core::marker::PhantomData,
+        }
+    }
+}
+
+impl<'a> ARPBuilder<'a, builder::TargetHardwareAddress> {
+    pub fn target_hardware_address(
+        mut self,
+        address: &[u8],
+    ) -> ARPBuilder<'a, builder::TargetProtocolAddress> {
+        self.buf[self.pos..self.pos + address.len()].copy_from_slice(address);
+        self.pos += address.len();
+        ARPBuilder {
+            buf: self.buf,
+            pos: self.pos,
+            _phantom: core::marker::PhantomData,
+        }
+    }
+}
+
+impl<'a> ARPBuilder<'a, builder::TargetProtocolAddress> {
+    pub fn target_protocol_address(mut self, address: &[u8]) -> ARP<'a> {
+        self.buf[self.pos..self.pos + address.len()].copy_from_slice(address);
+        self.pos += address.len();
+        ARP {
+            bytes: &self.buf[..self.pos],
+        }
+    }
+}
+
+pub(crate) mod builder {
+    pub trait Step {}
+
+    pub struct HardwareAddressSpace;
+    impl Step for HardwareAddressSpace {}
+
+    pub struct ProtocolAddressSpace;
+    impl Step for ProtocolAddressSpace {}
+
+    pub struct HardwareAddressLength;
+    impl Step for HardwareAddressLength {}
+
+    pub struct ProtocolAddressLength;
+    impl Step for ProtocolAddressLength {}
+
+    pub struct Opcode;
+    impl Step for Opcode {}
+
+    pub struct SenderHardwareAddress;
+    impl Step for SenderHardwareAddress {}
+
+    pub struct SenderProtocolAddress;
+    impl Step for SenderProtocolAddress {}
+
+    pub struct TargetHardwareAddress;
+    impl Step for TargetHardwareAddress {}
+
+    pub struct TargetProtocolAddress;
+    impl Step for TargetProtocolAddress {}
+}
+
+pub fn reply_writer(
+    recv_bytes: &[u8],
+    sha: MACAddress,
+) -> impl FnOnce(&mut [u8]) -> usize + use<'_> {
+    let request = ARP::from_bytes(recv_bytes);
+    let sha: [u8; 6] = sha.into();
+
+    move |buf| {
+        ARPBuilder::new(buf)
+            .hardware_address_space(request.hardware_address_space())
+            .protocol_address_space(request.protocol_address_space())
+            .hardware_address_length(request.hardware_address_length())
+            .protocol_address_length(request.protocol_address_length())
+            .opcode(Opcode::Reply)
+            .sender_hardware_address(&sha)
+            .sender_protocol_address(request.target_protocol_address())
+            .target_hardware_address(request.sender_hardware_address())
+            .target_protocol_address(request.target_protocol_address())
+            .len()
     }
 }
